2326e26d548a2bf2711494c09cc60ea57242f075
AkiraKhoa <125184970+AkiraKhoa@users.noreply.github.com>
2023-09-12 23:08:43 +0700

    Update touhoubutNot.py

--- touhoubutNot.py
+++ touhoubutNot.py
@@ -46,22 +46,28 @@ def draw_enemies():
     for enemy in enemies:
         pygame.draw.rect(screen, WHITE, (enemy[0], enemy[1], ENEMY_SIZE, ENEMY_SIZE))
 
-# Function to move and update positions
+# Updated spawn_enemy function
+def spawn_enemy():
+    x = random.randint(0, SCREEN_WIDTH - ENEMY_SIZE)
+    y = 0  # Spawn enemies at the top of the screen
+    enemy_health = ENEMY_HEALTH
+    enemies.append([x, y, 0, enemy_health])  # The new 0 represents the initial horizontal enemy movement
+
+# Updated move_objects function
 def move_objects():
     global player_x, player_y, player_hp
 
-    # Move bullets
-    for bullet in bullets:
-        bullet[0] += bullet[2] * BULLET_SPEED
-        bullet[1] += bullet[3] * BULLET_SPEED
+    # Move bullets and remove them if they go out of screen
+    bullets[:] = [(bullet[0] + bullet[2] * BULLET_SPEED, bullet[1] + bullet[3] * BULLET_SPEED, bullet[2], bullet[3])
+                  for bullet in bullets if 0 <= bullet[0] < SCREEN_WIDTH and 0 <= bullet[1] < SCREEN_HEIGHT]
 
-    # Move enemies
+    # Move enemies and handle their behavior
     for enemy in enemies:
         enemy[0] += enemy[2] * BULLET_SPEED  # Adjust enemy's x position
 
-        # Check if enemies reached the bottom half of the screen
-        if enemy[1] > SCREEN_HEIGHT // 2:
-            enemy[1] = SCREEN_HEIGHT // 2  # Limit enemies to the bottom half
+        # Update enemy's vertical position to make them move downward
+        if enemy[1] < SCREEN_HEIGHT // 2:
+            enemy[1] += 1  # Move downward until reaching the bottom half of the screen
 
         # Check if bullets hit enemies
         for bullet in bullets:
@@ -82,12 +88,6 @@ def move_objects():
     if keys[pygame.K_d] and player_x < SCREEN_WIDTH - PLAYER_SIZE:
         player_x += PLAYER_SPEED
 
-# Function to spawn enemies
-def spawn_enemy():
-    x = random.randint(0, SCREEN_WIDTH - ENEMY_SIZE)
-    y = -ENEMY_SIZE
-    enemy_health = ENEMY_HEALTH
-    enemies.append([x, y, 0, enemy_health])  # The new 0 represents the initial horizontal enemy movement
 
 # Cooldown timer for player shots
 shot_cooldown = 0
@@ -113,7 +113,7 @@ while running:
         shot_cooldown -= 1
 
     # Spawn enemies randomly
-    if random.randint(1, 100) < 99:
+    if random.randint(1, 100) < 20:
         spawn_enemy()
 
     # Clear the screen
